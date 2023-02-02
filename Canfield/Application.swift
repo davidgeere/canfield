1e4c7e15b46fcc93d8cbd842a1618a6857bc509a
David Geere <david@geere.org>
2023-02-02 10:02:33 -0800

    Initial Commit
    I created a new project to reduce complexity. I figured out a new way of doing the game and wanted to only work with the smallest amount of code.

---
+++ Canfield/Application.swift
@@ -0,0 +1,17 @@
+//
+//  CanfieldApp.swift
+//  Canfield
+//
+//  Created by David Geere on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            TableView()
+        }
+    }
+}
---
+++ TableLayoutView.swift
@@ -0,0 +1,24 @@
+//
+//  TableLayoutView.swift
+//  Canfield
+//
+//  Created by David Geere on 2/2/23.
+//
+
+import SwiftUI
+
+struct TableLayoutView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("table"))
+    }
+}
+
+struct TableLayoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        TableLayoutView()
+    }
+}
